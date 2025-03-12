01efed9eded9b72460d9db15ec2427612bc320b0
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-03-12 12:44:20 +0900

    Cosmetic

--- src/macros.rs
+++ src/macros.rs
@@ -19,6 +19,8 @@ macro_rules! format_args {
 #[macro_export]
 macro_rules! best_fitting {
     ($least_expanded:expr, $($tail:expr),+ $(,)?) => {
-        $crate::builders::BestFitting::from_arguments_unchecked($crate::format_args!($least_expanded, $($tail),+))
+        $crate::builders::BestFitting::from_arguments_unchecked(
+            $crate::format_args!($least_expanded, $($tail),+)
+        )
     };
 }
