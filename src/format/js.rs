d75f55f56cc6335b8b949d2ad817fdd721c9b3b1
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-03-17 22:22:15 +0900

    Wip minimum formatting

--- src/format/js.rs
+++ src/format/js.rs
@@ -1,4 +1,5 @@
 use oxc_ast::ast::*;
+use oxc_span::GetSpan;
 
 use crate::base_formatter::{Buffer, FormatResult, builders::*};
 use crate::{FormatNodeRule, JsFormatter, write};
@@ -7,16 +8,27 @@ use crate::{FormatNodeRule, JsFormatter, write};
 pub(crate) struct FormatProgram;
 impl FormatNodeRule<Program<'_>> for FormatProgram {
     fn fmt_fields(&self, node: &Program, f: &mut JsFormatter) -> FormatResult<()> {
-        let Program { span, body, .. } = node;
-        let _ = body;
+        let Program { body, .. } = node;
 
-        write!(
-            f,
-            [
-                text("/* TODO */"),
-                hard_line_break(),
-                dynamic_text(span.source_text(f.context().source_text()))
-            ]
-        )
+        let mut join = f.join_nodes_with_hardline();
+
+        for stmt in body {
+            // join.entry(&(), &stmt.format());
+            join.entry(
+                &(),
+                &format_with(|f: &mut JsFormatter| {
+                    write!(
+                        f,
+                        [
+                            text("// TODO: Statement"),
+                            hard_line_break(),
+                            dynamic_text(stmt.span().source_text(f.context().source_text()))
+                        ]
+                    )
+                }),
+            );
+        }
+
+        join.finish()
     }
 }
