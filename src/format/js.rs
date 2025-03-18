4527e4748b4ca2920d7deae0356ce10d6083078c
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-03-18 16:59:04 +0900

    Wip

--- src/format/js.rs
+++ src/format/js.rs
@@ -1,8 +1,8 @@
 use oxc_ast::ast::*;
 use oxc_span::GetSpan;
 
-use crate::base_formatter::{Buffer, FormatResult, builders::*};
-use crate::{FormatNodeRule, JsFormatter, write};
+use crate::base_formatter::{Buffer, Format, FormatResult, FormatRule, builders::*};
+use crate::{AsFormat, FormatNodeRule, JsFormatContext, JsFormatter, write};
 
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatProgram;
@@ -10,25 +10,54 @@ impl FormatNodeRule<Program<'_>> for FormatProgram {
     fn fmt_fields(&self, node: &Program, f: &mut JsFormatter) -> FormatResult<()> {
         let Program { body, .. } = node;
 
-        let mut join = f.join_nodes_with_hardline();
+        write!(f, [text("// TODO: Program"), hard_line_break()])?;
 
         for stmt in body {
-            // join.entry(&(), &stmt.format());
-            join.entry(
-                &(),
-                &format_with(|f: &mut JsFormatter| {
-                    write!(
-                        f,
-                        [
-                            text("// TODO: Statement"),
-                            hard_line_break(),
-                            dynamic_text(stmt.span().source_text(f.context().source_text()))
-                        ]
-                    )
-                }),
-            );
+            write!(f, [stmt.format()])?;
         }
+        Ok(())
+        // let mut join = f.join_nodes_with_hardline();
+        // for stmt in body {
+        //     join.entry(&(), &stmt.format());
+        // }
 
-        join.finish()
+        // join.finish()
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub(crate) struct FormatStatement;
+impl FormatRule<Statement<'_>, JsFormatContext<'_>> for FormatStatement {
+    fn fmt(&self, node: &Statement, f: &mut JsFormatter) -> FormatResult<()> {
+        match node {
+            Statement::VariableDeclaration(stmt) => stmt.format().fmt(f),
+            _ => write!(
+                f,
+                [
+                    text("// TODO: Statement"),
+                    hard_line_break(),
+                    dynamic_text(node.span().source_text(f.context().source_text()))
+                ]
+            ),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub(crate) struct FormatVariableDeclaration;
+impl FormatNodeRule<VariableDeclaration<'_>> for FormatVariableDeclaration {
+    fn fmt_fields(&self, node: &VariableDeclaration, f: &mut JsFormatter) -> FormatResult<()> {
+        let VariableDeclaration { kind, .. } = node;
+
+        write!(
+            f,
+            [
+                text("// TODO: VariableDeclaration @"),
+                text(kind.as_str()),
+                hard_line_break(),
+            ]
+        )?;
+
+        Ok(())
     }
 }
